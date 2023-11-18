b4e113cd96bf8aa6204edf417a10d6d9a34dc7b7
Elisson Guímel da Silva <guigolawliet13@gmail.com>
2023-11-17 21:39:30 -0300

    config: fix deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 require 'factory_bot'
 
-admin_user = FactoryBot.create(:user, email: 'admin@dev.com.br', role: :administrator, first_name: 'Desenvolvedor', last_name: 'Administrador')
+admin_user = FactoryBot.create(:user, email: 'admin@dev.com.br', role: :administrator, first_name: 'Desenvolvedor', last_name: 'Administrador', password: 'Teste@1234', password_confirmation: 'Teste@1234')
 FactoryBot.create_list(:user, 9)
